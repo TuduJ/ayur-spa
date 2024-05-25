0f4ef33064f461464958e76ba2a022ee7084c39b
JitendraTD <jitendranath.tudu@ocacademy.in>
2024-05-25 14:16:34 +0530

    image redirect link

--- src/components/Header.js
+++ src/components/Header.js
@@ -37,10 +37,13 @@ const items = [
 ];
 
 const Header = () => {
+
   return (
     <div className="bg-baige-100 p-10 width-100-per fixed top-0 zIndex-1">
       <div className="pl-100 sm-pl-10 pr-140 sm-pr-20 flex fd-row items-center jc-space-between">
-        <img src={BrandLogo} alt="Brand Logo" className="sm-width-50 "/>
+        <Link to="/">
+          <img src={BrandLogo} alt="Brand Logo" className="sm-width-50 "/>
+        </Link>
         <div className="width-70-per font-bold-700 flex jc-space-between sm-hidden">
           <p className="font-charcoal header-link"><Link to="/" className="font-charcoal header-link">HOME</Link></p>
           <p className="font-charcoal header-link"><Link to="/" className="font-charcoal header-link"><a href="#about">ABOUT</a></Link></p>
